9f1feff1ddde248493600e5e151a26e80f51087d
zeronesun <20874567+zeronesun@users.noreply.github.com>
2019-09-03 13:55:11 +0800

    add rc4_test.c

---
+++ rc4/rc4_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <memory.h>
+#include "rc4.h"
+
+void print_hex(BYTE str[], int len)
+{
+	int idx;
+
+	for(idx = 0; idx < len; idx++)
+		printf("%02x", str[idx]);
+}
+
+
+
+int main()
+{   
+    char text[] = "14564561313213215665423";
+    Rc4EncryptText(text);
+    return 0;
+}
