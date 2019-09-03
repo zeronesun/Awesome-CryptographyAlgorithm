e9dd39b78e8fd9729ea18fb45fbe60ddfc58a171
zeronesun <slf_003@163.com>
2019-09-03 14:25:45 +0800

    shift+alt+f 代码格式化

--- md2/md2.c
+++ md2/md2.c
@@ -30,31 +30,34 @@ static const BYTE s[256] = {
 	8, 12, 189, 177, 74, 120, 136, 149, 139, 227, 99, 232, 109, 233,
 	203, 213, 254, 59, 0, 29, 57, 242, 239, 183, 14, 102, 88, 208, 228,
 	166, 119, 114, 248, 235, 117, 75, 10, 49, 68, 80, 180, 143, 237,
-	31, 26, 219, 153, 141, 51, 159, 17, 131, 20
-};
+	31, 26, 219, 153, 141, 51, 159, 17, 131, 20};
 
 /*********************** FUNCTION DEFINITIONS ***********************/
 void md2_transform(MD2_CTX *ctx, BYTE data[])
 {
-	int j,k,t;
+	int j, k, t;
 
 	//memcpy(&ctx->state[16], data);
-	for (j=0; j < 16; ++j) {
+	for (j = 0; j < 16; ++j)
+	{
 		ctx->state[j + 16] = data[j];
-		ctx->state[j + 32] = (ctx->state[j+16] ^ ctx->state[j]);
+		ctx->state[j + 32] = (ctx->state[j + 16] ^ ctx->state[j]);
 	}
 
 	t = 0;
-	for (j = 0; j < 18; ++j) {
-		for (k = 0; k < 48; ++k) {
+	for (j = 0; j < 18; ++j)
+	{
+		for (k = 0; k < 48; ++k)
+		{
 			ctx->state[k] ^= s[t];
 			t = ctx->state[k];
 		}
-		t = (t+j) & 0xFF;
+		t = (t + j) & 0xFF;
 	}
 
 	t = ctx->checksum[15];
-	for (j=0; j < 16; ++j) {
+	for (j = 0; j < 16; ++j)
+	{
 		ctx->checksum[j] ^= s[data[j] ^ t];
 		t = ctx->checksum[j];
 	}
@@ -64,9 +67,9 @@ void md2_init(MD2_CTX *ctx)
 {
 	int i;
 
-	for (i=0; i < 48; ++i)
+	for (i = 0; i < 48; ++i)
 		ctx->state[i] = 0;
-	for (i=0; i < 16; ++i)
+	for (i = 0; i < 16; ++i)
 		ctx->checksum[i] = 0;
 	ctx->len = 0;
 }
@@ -75,10 +78,12 @@ void md2_update(MD2_CTX *ctx, const BYTE data[], size_t len)
 {
 	size_t i;
 
-	for (i = 0; i < len; ++i) {
+	for (i = 0; i < len; ++i)
+	{
 		ctx->data[ctx->len] = data[i];
 		ctx->len++;
-		if (ctx->len == MD2_BLOCK_SIZE) {
+		if (ctx->len == MD2_BLOCK_SIZE)
+		{
 			md2_transform(ctx, ctx->data);
 			ctx->len = 0;
 		}
--- rc4/rc4_test.c
+++ rc4/rc4_test.c
@@ -4,16 +4,14 @@
 
 void print_hex(BYTE str[], int len)
 {
-	int idx;
+    int idx;
 
-	for(idx = 0; idx < len; idx++)
-		printf("%02x", str[idx]);
+    for (idx = 0; idx < len; idx++)
+        printf("%02x", str[idx]);
 }
 
-
-
 int main()
-{   
+{
     char text[] = "14564561313213215665423";
     Rc4EncryptText(text);
     return 0;
