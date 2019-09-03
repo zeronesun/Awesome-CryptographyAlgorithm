22f17cd1fc2b8200839ec104c2fd3d04f30e5abb
zeronesun <slf_003@163.com>
2019-09-03 13:57:11 +0800

    edit debug info

---
+++ rc4/rc4.h
@@ -0,0 +1,20 @@
+/*********************************************************************
+* Filename:   rc4.h
+* Details:    Defines the API for the corresponding rc4 implementation.
+*********************************************************************/
+#ifndef RC4_H
+#define RC4_H
+
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+
+#define MAX 65534
+
+typedef unsigned char BYTE;             // 8-bit byte
+
+void Rc4EncryptText(char *text);
+
+
+#endif   // RC4_H
